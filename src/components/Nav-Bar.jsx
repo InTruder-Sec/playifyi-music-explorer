4471b6e1b5886508febf2acf56b565e8c59248d5
= <dhakatedeep909@gmail.com>
2024-01-08 00:48:30 +0530

    Fix: Navbar buttons

--- src/components/Nav-Bar.jsx
+++ src/components/Nav-Bar.jsx
@@ -1,48 +1,33 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import "./Nav-Bar.css";
-import { previousWindow, nxtWindow } from "../App";
-import { MainWindow, SongsData } from "./Main";
-
-function ForwardWindow() {
-  // let value = useContext(MainWindow);
-  // value(() => {
-  //   return {
-  //     home: false,
-  //     playwin: true,
-  //   };
-  // });
-  // document.getElementById("next--arrow").style.cursor = "not-allowed";
-  // document.getElementById("next--arrow").style.opacity = "0.5";
-  // document.getElementById("prev--arrow").style.cursor = "pointer";
-  // document.getElementById("prev--arrow").style.opacity = "1";
-}
-
-function PreWindow() {
-  const first = useContext(MainWindow);
-  alert(first);
-
-  // let value = useContext(MainWindow);
-  // value(() => {
-  //   return {
-  //     home: true,
-  //     playwin: false,
-  //   };
-  // });
-  // document.getElementById("next--arrow").style.cursor = "pointer";
-  // document.getElementById("next--arrow").style.opacity = "1";
-  // document.getElementById("prev--arrow").style.cursor = "not-allowed";
-  // document.getElementById("prev--arrow").style.opacity = "0.5";
-}
+import { MainWindow } from "./Main";
 
 function NavBar() {
+  const setWindow = useContext(MainWindow);
+
+  function PreWindow() {
+    setWindow(() => {
+      return {
+        home: true,
+        playwin: false,
+      };
+    });
+    document.getElementById("prev--arrow").style.opacity = "0.5";
+    document.getElementById("prev--arrow").style.cursor = "not-allowed";
+  }
+
   return (
     <div className="NavBar">
       <div className="change--display">
         <div className="change--arrow" id="prev--arrow" onClick={PreWindow}>
           {"<"}
         </div>
-        <div className="change--arrow" id="next--arrow" onClick={ForwardWindow}>
+        <div
+          className="change--arrow"
+          id="next--arrow"
+          style={{ display: "none" }}
+          // onClick={ForwardWindow}
+        >
           {">"}
         </div>
       </div>
